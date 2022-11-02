accbc46c9d6df0c54f16822bd1983ac1292328c9
david.Ruiz <david.ruiz@software.com>
2022-11-02 07:44:44 -0500

    Usuario,Cliente, Comercio, Empleado

--- src/app/modules/home/components/HomeComponent.jsx
+++ src/app/modules/home/components/HomeComponent.jsx
@@ -1,6 +1,18 @@
-import { Grid, Typography } from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { Alert, Grid, Stack, Typography } from "@mui/material";
+import { authstatusTypes } from "../../../../store";
+import { RolTypes } from "../../../types";
 
 export const HomeComponent = ({ navBarWidth = 58 }) => {
+  const { status, user } = useSelector(state => state.auth);
+  const CUSTOMERAUTHENTICATED = useMemo(() =>
+    status === authstatusTypes.AUTHENTICATED && user?.rol?.id === RolTypes.customer,
+    [status, user])
+
+  console.log('user', user)
+  console.log('CUSTOMERAUTHENTICATED', CUSTOMERAUTHENTICATED)
+
   return (
     <Grid container spacing={0} justifyContent="center"
       sx={{
@@ -10,6 +22,13 @@ export const HomeComponent = ({ navBarWidth = 58 }) => {
         alignItems: { xs: 'start', md: 'center' }
       }}
     >
+      {CUSTOMERAUTHENTICATED &&
+        <Stack sx={{ width: '100%' }} spacing={2}>
+          <Alert severity="warning">
+            {user.name}, aún no cuantas con un Comercio, en la opción: Negocio podras diligenciar los campos requeridos.
+          </Alert>
+        </Stack>
+      }
       <Typography>HomeComponent</Typography>
     </Grid>
   )
