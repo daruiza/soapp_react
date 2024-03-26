69c09ac7e0a7f393ffb5119603e457cb2d22672f
daruiza <daruiza@gmail.com>
2024-03-26 17:09:28 -0500

    correccion evidence to employeeevidence

--- src/store/evidence/evidenceThunks.jsx
+++ src/store/evidence/evidenceThunks.jsx
@@ -4,7 +4,7 @@ export const evidenceStore = ({ form }) => {
     return async (dispatch) => {
         const { soappLaravelApi } = useAppSoappLaravel(dispatch);
         if (form) {
-            return soappLaravelApi.post(`api/evidence/store`, form);
+            return soappLaravelApi.post(`api/employeeevidence/store`, form);
         }
     }
 }
@@ -13,7 +13,7 @@ export const evidenceUpdate = ({ form }) => {
     return async (dispatch) => {
         const { soappLaravelApi } = useAppSoappLaravel(dispatch);
         if (form && form.id) {
-            return soappLaravelApi.put(`api/evidence/update/${form.id}`, form);
+            return soappLaravelApi.put(`api/employeeevidence/update/${form.id}`, form);
         }
     }
 }
@@ -22,7 +22,7 @@ export const showByEmpoyeeReportId = ({ form = {} }) => {
     return async (dispatch) => {
         const { soappLaravelApi } = useAppSoappLaravel(dispatch);
         if (form && form.id) {
-            return soappLaravelApi.get(`api/evidence/showbyempoyeereportid/${form.id}`, { params: { ...form } });
+            return soappLaravelApi.get(`api/employeeevidence/showbyempoyeereportid/${form.id}`, { params: { ...form } });
         }
     }
 }
@@ -31,7 +31,7 @@ export const deleteEvidenceId = ({ form = {} }) => {
     return async (dispatch) => {
         const { soappLaravelApi } = useAppSoappLaravel(dispatch);
         if (form && form.id) {
-            return soappLaravelApi.delete(`api/evidence/destroy/${form.id}`);
+            return soappLaravelApi.delete(`api/employeeevidence/destroy/${form.id}`);
         }
     }
 }
